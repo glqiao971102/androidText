e2d5de7ee9279d9a01bd08b19294e42b825b5d84
glqiao971102 <44012470+glqiao971102@users.noreply.github.com>
2020-04-07 09:38:34 +0800

    add button and log function

--- app/src/main/java/com/example/ilovecem300/MainActivity.java
+++ app/src/main/java/com/example/ilovecem300/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.ilovecem300;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private static final String TAG = "HelloMeActivity";
+
+    public void onButtonClick(View view) {
+        EditText editName = (EditText) findViewById(R.id.editText);
+        TextView textDisplay = (TextView) findViewById(R.id.textViewDisplay);
+        String nameToDisplay = editName.getText().toString();
+        textDisplay.setText("Hello " + nameToDisplay);
+        Log.d(TAG, nameToDisplay + ", this is your system log!");
+    }
+
 }
